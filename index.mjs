b521680da0a151ebd4c6089189867246fdb4cd59
Oliver Rindholt <olr@daxmedia.eu>
2025-08-12 14:27:52 +0200

    refactor: changed index config file to mjs

---
+++ index.mjs
@@ -0,0 +1,14 @@
+/**
+ * @type {import("prettier").Config}
+ */
+const config = {
+	arrowParens: "avoid",
+	useTabs: true,
+	singleQuote: false,
+	jsxSingleQuote: false,
+	semi: true,
+	tabWidth: 2,
+	trailingComma: "none",
+};
+
+export default config;
